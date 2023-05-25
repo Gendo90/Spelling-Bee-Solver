756682c023e97a6a3a698685307306fce38d2d86
Patrick Gendotti <patrick.gendotti@gmail.com>
2023-05-25 09:54:10 -0700

    feat: Update solver.js to use asynchronous loading of the wordlist

--- js/solver.js
+++ js/solver.js
@@ -1,7 +1,10 @@
-const wordlist = new Set();
+const wordList = new Set();
 
-fetch("./data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordlist.add(word)))
-// fetch("./data/dictionary.json").then(a => a.json()).then(result => console.log(Object.keys(result)))//.forEach(word => wordlist.add(word)))
+async function setWordList(wordList) {
+    await fetch("./data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordlist.add(word)))
 
-console.log(wordlist.size) // note that this is only about 86k and needs to be around 171k for the current English dictionary size...
+    console.log(wordList.size) // note that this is only about 86k and needs to be around 171k for the current English dictionary size...
 // will need to get a better word list at some point!
+}
+
+setWordList(wordList)
