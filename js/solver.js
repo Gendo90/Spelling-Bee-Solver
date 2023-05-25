c56b617bdbb295539535c3d247053e4104bc550c
Patrick Gendotti <patrick.gendotti@gmail.com>
2023-05-25 09:47:03 -0700

    feat: Now correctly store words in a set from the dictionary data

--- js/solver.js
+++ js/solver.js
@@ -1,6 +1,7 @@
 const wordlist = new Set();
 
-// fetch("./data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordlist.add(word)))
-fetch("./data/dictionary.json").then(a => a.json()).then(result => console.log(Object.keys(result)))//.forEach(word => wordlist.add(word)))
+fetch("./data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordlist.add(word)))
+// fetch("./data/dictionary.json").then(a => a.json()).then(result => console.log(Object.keys(result)))//.forEach(word => wordlist.add(word)))
 
-console.log(wordlist.size)
+console.log(wordlist.size) // note that this is only about 86k and needs to be around 171k for the current English dictionary size...
+// will need to get a better word list at some point!
