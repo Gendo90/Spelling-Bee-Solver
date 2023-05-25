e411ac265c6d44eb55ffa813878965380f6db3f8
Patrick Gendotti <patrick.gendotti@gmail.com>
2023-05-25 09:58:03 -0700

    fix: Fix typo in wordList variable name in solver.js

--- js/solver.js
+++ js/solver.js
@@ -1,7 +1,7 @@
 const wordList = new Set();
 
 async function setWordList(wordList) {
-    await fetch("./data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordlist.add(word)))
+    await fetch("./data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordList.add(word)))
 
     console.log(wordList.size) // note that this is only about 86k and needs to be around 171k for the current English dictionary size...
 // will need to get a better word list at some point!
