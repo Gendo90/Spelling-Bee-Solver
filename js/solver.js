a03c3b1c8d9916bbd9b3807e3cc5fdd0b55d168f
Patrick Gendotti <patrick.gendotti@gmail.com>
2023-05-25 09:39:38 -0700

    fix: Update dictionary source path to work with Github Pages directory structure

--- js/solver.js
+++ js/solver.js
@@ -1,5 +1,5 @@
 const wordlist = new Set();
 
-fetch("../data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordlist.add(word)))
+fetch("./data/dictionary.json").then(a => a.json()).then(result => Object.keys(result).forEach(word => wordlist.add(word)))
 
 console.log(wordlist.size)
